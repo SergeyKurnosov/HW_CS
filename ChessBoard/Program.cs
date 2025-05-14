a973a201460f363c5a8d98e6313e3c9f757905a6
SergeyKurnosov <kurnosovsergej123@mail.ru>
2025-05-14 14:53:52 +1000

    H_W_12.05.2025

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoard
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.Write("Введите размер фигуры: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+
+			for (int i = 0; i < n * 8; i++)
+			{
+
+				char s1 = ' ', s2 = ' ';
+				if ((i >= 0 && i < n) || (i >= n * 2 * (i / (n * 2)) && i < n * 2 * (i / (n * 2)) + n))
+					s1 = '*';
+				else
+					s2 = '*';
+
+				for (int j = 0; j < 4; j++)
+				{
+
+					for (int k = 0; k < n; k++)
+						Console.Write($"{s1,2}");
+
+					for (int k = 0; k < n; k++)
+						Console.Write($"{s2,2}");
+
+				}
+
+				Console.WriteLine();
+
+
+			}
+
+		}
+	}
+}
---
+++ Rhombus/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Rhombus
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.Write("Введите размер фигуры: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+
+			for(int i = 0; i < n*2; ++i)
+			{
+				int temp = i < n ? n - i - 1 : i - n;
+
+				for (int j = 0; j < temp;)
+				{
+					Console.Write(" ");
+					++j;
+				}
+					Console.Write(i < n ? "/" : "\\");
+
+				temp = i < n ? i * 2 : (n*2) - 2 - (temp * 2);
+				for (int j = 0; j < temp;)
+				{
+					Console.Write(" ");
+					++j;
+				}
+				Console.Write(i < n ? "\\" : "/");
+				Console.WriteLine();
+
+			}
+
+
+			///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			//for (int i = 0; i < n; i++)
+			//{
+			//	for (int j = i; j < n; j++) Console.Write(" "); Console.Write("/");
+			//	for (int j = 0; j < i * 2; j++) Console.Write(" "); Console.Write("\\");
+			//	Console.WriteLine();
+			//}
+
+			//for (int i = 0; i < n; i++)
+			//{
+			//	for (int j = 0; j <= i; j++) Console.Write(" "); Console.Write("\\");
+			//	for (int j = 0; j < (n - i - 1) * 2; j++) Console.Write(" "); Console.Write("/");
+			//	Console.WriteLine();
+			//}
+
+		}
+	}
+}
